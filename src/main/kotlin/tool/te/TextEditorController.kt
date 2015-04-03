aa7929530b6f9500495e1c07592236c10d097293
kuwalab <arata.shinichi@gmail.com>
2015-04-04 05:40:20 +0900

    ファイルを開くときのエンコードの指定

--- src/main/kotlin/tool/te/TextEditorController.kt
+++ src/main/kotlin/tool/te/TextEditorController.kt
@@ -3,16 +3,25 @@ package tool.te
 import javafx.application.Platform
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.scene.control.TextArea
 import javafx.stage.FileChooser
 import javafx.stage.Stage
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
 
 class TextEditorController {
     var stage: Stage? = null
 
+    FXML var textArea: TextArea? = null
+
     FXML fun handleOpen() {
         val fc = FileChooser()
         fc.setTitle("ファイルを開く")
-        fc.showOpenDialog(stage)
+        val file = fc.showOpenDialog(stage)
+        if (file == null) return
+        Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8).use {
+            textArea!!.setText(it.readText())
+        }
     }
 
     FXML fun handleExit(event: ActionEvent) {
