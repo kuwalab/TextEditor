c791a9f76b6d582518aa7224a8f356609aba234f
kuwalab <arata.shinichi@gmail.com>
2015-04-11 08:09:16 +0900

    CharsetsをKotlinのライブラリのものにする

--- src/main/kotlin/tool/te/TextEditorController.kt
+++ src/main/kotlin/tool/te/TextEditorController.kt
@@ -3,11 +3,18 @@ package tool.te
 import javafx.application.Platform
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.Alert
 import javafx.scene.control.TextArea
 import javafx.stage.FileChooser
+import javafx.stage.Modality
 import javafx.stage.Stage
+import javafx.stage.StageStyle
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
+import javax.print.attribute.standard.DialogTypeSelection
 
 class TextEditorController {
     var stage: Stage? = null
@@ -19,10 +26,30 @@ class TextEditorController {
         fc.setTitle("ファイルを開く")
         val file = fc.showOpenDialog(stage)
         if (file == null) return
-        textArea!!.setText(file.readText(StandardCharsets.UTF_8))
+        textArea!!.setText(file.readText(Charsets.UTF_8))
     }
 
-    FXML fun handleExit(event: ActionEvent) {
+    FXML fun handleExit() {
+        val alert = Alert(Alert.AlertType.CONFIRMATION)
+        alert.setTitle("終了しますか")
+        alert.getDialogPane().setHeaderText("終了しますか")
+        alert.getDialogPane().setContentText("")
+        alert.showAndWait()
+
         Platform.exit()
+    }
+
+    FXML fun handleConfig() {
+        val fxmlLoader: FXMLLoader = FXMLLoader(javaClass<TextEditor>().getResource("ConfigDialog.fxml"))
+        val root: Parent = fxmlLoader.load()
+
+        val scene = Scene(root)
+        val confirmDialog = Stage(StageStyle.UTILITY)
+        confirmDialog.setScene(scene);
+        confirmDialog.initOwner(stage);
+        confirmDialog.initModality(Modality.WINDOW_MODAL);
+        confirmDialog.setResizable(false);
+        confirmDialog.setTitle("設定")
+        confirmDialog.showAndWait()
     }
 }
